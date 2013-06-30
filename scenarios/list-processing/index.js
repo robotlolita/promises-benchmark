157f9ba9bdb5316f6cc5da086111c341b6c1fd04
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-06-29 23:02:45 -0300

    Adds pinky & when.

--- scenarios/list-processing/index.js
+++ scenarios/list-processing/index.js
@@ -6,5 +6,7 @@ var dirname     = path.join(__dirname, '../../fixtures/files')
 benchmark.suite('List processing (no noise)', function(bench) {
 
   bench('Q', require('./q')(dirname, 0))
+  bench('Pinky', require('./pinky')(dirname, 0))
+  bench('When', require('./when')(dirname, 0))
 
 })
