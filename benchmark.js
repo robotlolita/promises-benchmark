d98f09c76e2614c4dd2cbc728f6723783e05ad1b
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-07-06 11:15:17 -0300

    Saves results from tests in a discrete stream of events over time.

--- benchmark.js
+++ benchmark.js
@@ -1,15 +1,39 @@
 var Benchmark = require('benchmark')
+var fs        = require('fs')
+// sync so we don't fuck up with other benchs
+var read      = fs.readFileSync
+var write     = fs.writeFileSync
+var exists    = fs.existsSync
 
 var queue   = []
 var running = null
+var log     = console.log.bind(console)
 
 function suite(name, block) {
   var s = new Benchmark.Suite(name)
   block(bench(s))
 
-  if (!running)  run(s)
+  if (!running)  run(s, saveAs(name))
   else           queue.push(s)
 }
+
+function hasErrors(data) {
+  return data.some(function(a){ return a.error })
+}
+
+function saveAs(name){ return function(data) {
+  if (hasErrors(data))  return log('Not saving because errors occurred.')
+
+  var previous = exists('bench.json')?  read('bench.json', 'utf-8')
+               : /* otherwise */        '[]'
+
+  var resultStream = JSON.parse(previous)
+  resultStream.push({ name:  name
+                    , date:  new Date
+                    , stats: data })
+
+  write('bench.json', JSON.stringify(resultStream))
+}}
 
 function bench(suite){ return function(name, code) {
   suite.add(name, { defer: true, fn: code })
@@ -22,9 +46,15 @@ function slowest(suite) {
   return suite.filter('slowest').pluck('name') }
 
 function run(suite, cb) {
-  var err = 0
+  var err     = 0
+  var results = []
   suite.on('cycle', function(event) {
                       var bench = event.target
+                      results.push({ name      : bench.name
+                                   , error     : bench.error
+                                   , time      : bench.hz
+                                   , variation : bench.stats.rme })
+
                       if (bench.error) console.error( '\x1B[0;31m' + ++err + ')'
                                                     , String(bench)
                                                     , bench.error.message
@@ -32,20 +62,20 @@ function run(suite, cb) {
                                                     , bench.error.stack || ''
                                                     , '\x1B[0m')
 
-                      else             console.log('››', String(bench)) })
+                      else             log('››', String(bench)) })
 
   suite.on('complete', function() {
-                         console.log('---'
-                                    ,'\nFastest:', fastest(this).join(', ')
-                                    ,'\nSlowest:', slowest(this).join(', '))
-                         if (cb)  cb()
+                         log('---'
+                            ,'\nFastest:', fastest(this).join(', ')
+                            ,'\nSlowest:', slowest(this).join(', '))
+                         if (cb)  cb(results)
 
                          running  = null
                          var next = queue.shift()
-                         if (next)  run(next) })
+                         if (next)  run(next, cb) })
 
-  console.log('\x1B[0;36m\n:: Benchmarks for:', suite.name)
-  console.log('   Sit back, this can take a while.\x1B[0m\n---')
+  log('\x1B[0;36m\n:: Benchmarks for:', suite.name)
+  log('   Sit back, this can take a while.\x1B[0m\n---')
 
   running = suite
 
